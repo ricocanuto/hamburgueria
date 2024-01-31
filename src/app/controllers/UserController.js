1d8d95a6499a07b028989b94496a1ec2eed27022
ricocanuto <ricardomoraescanuto@gmail.com>
2024-01-31 16:30:04 -0300

    users update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,20 @@
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
 
-import User from '../models/user'
+import User from ' ../models/User'
 
 class UserController {
     async store(request, response) {
+
+        const schema =Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+        })
+
+        console.log(await schema.isValid(request.body))
+
         const { name, email, password_hash, admin } = request.body
 
         const user = await User.create({
@@ -14,7 +25,7 @@ class UserController {
             admin,
         })
 
-        return response.json(user)
+        return response.status(201).json({ id: user.id, name, email, admin })
     }
 }
 
